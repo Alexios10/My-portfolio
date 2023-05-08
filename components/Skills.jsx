c4b09e5d2253f7e8bb8c2268e3ef4c26128d169c
Alexios10 <sattar_saud@hotmail.com>
2023-05-08 23:03:29 +0200

    updated the about me section and added new skill to the skills section

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -6,7 +6,7 @@ import jsImg from "../public/assets/skills/JavaScript.png";
 import reactImg from "../public/assets/skills/React.png";
 import tailImg from "../public/assets/skills/tailwind.png";
 import bootsImg from "../public/assets/skills/Bootstrap.png";
-import fireImg from "../public/assets/skills/Firebase.png";
+import nextImg from "../public/assets/skills/next.png";
 import gitImg from "../public/assets/skills/github1.png";
 
 const Skills = () => {
@@ -65,6 +65,17 @@ const Skills = () => {
           <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
+                <Image src={nextImg} width="64px" height="64px" alt="/" />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3 className="cursor-default">Next</h3>
+              </div>
+            </div>
+          </div>
+
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
                 <Image src={tailImg} width="64px" height="64px" alt="/" />
               </div>
               <div className="flex flex-col items-center justify-center">
@@ -80,17 +91,6 @@ const Skills = () => {
               </div>
               <div className="flex flex-col items-center justify-center">
                 <h3 className="cursor-default">Bootstrap</h3>
-              </div>
-            </div>
-          </div>
-
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image src={fireImg} width="64px" height="64px" alt="/" />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3 className="cursor-default">Firebase</h3>
               </div>
             </div>
           </div>
