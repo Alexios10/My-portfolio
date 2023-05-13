b11902f86b3b6855c48584ba631404e7e4ec57c2
Alexios10 <sattar_saud@hotmail.com>
2023-05-13 19:25:18 +0200

    Changed to new URL

--- pages/movix.jsx
+++ pages/movix.jsx
@@ -36,7 +36,7 @@ const Hulu = () => {
           </p>
           <button className="px-8 py-2 mt-4 mr-8">
             <a
-              href="https://movix-kappa-one.vercel.app/"
+              href="https://movix-self-nine.vercel.app/"
               target="_blank"
               rel="noopener noreferrer"
             >
